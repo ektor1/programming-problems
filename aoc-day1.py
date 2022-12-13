4c9761d73da81de825f0ed96f07b1a87775d1c67
Ektor Nikolinakos <hc.nlks@gmail.com>
2022-12-13 14:09:30 +0000

    aoc day 1

--- aoc-day1.py
+++ aoc-day1.py
@@ -4,30 +4,54 @@
 # each elf's inventory is separated by a line and each block is the calories of the items that an elf has brought 
 # Find how many calories are carried by the elf carrying the most calories
 
-def max_calories(my_file):
-    # open file in read mode
-    my_file = open(my_file, "r")
+# open file in read mode
+my_file = open("input1.txt", "r")
 
-    # read the file
+# read the file
     
-    data = my_file.read()
+data = my_file.read()
 
-    # split the data for each vertical space
-    calories = data.split("\n")
-    
+# split the data for each vertical space
+calories = data.split("\n")
+
+def max_calories(calories):    
     # store each elfs inventory in current. If current is larger than max
     # set max equal to current   
-    max = 0
     current = 0
+    max = 0
     for i in calories:
         if len(i) != 0:
             current += int(i)
 
-        elif len(i) == 0:
+        else:
             if current > max:
                 max = current
             current = 0
 
     return max
 
+# print(max_calories(calories))
 
+def top_3_calories(calories):
+    current = 0
+    top_1 = 0
+    top_2 = 0
+    top_3 = 0
+
+    for i in calories:
+        if len(i) != 0:
+            current += int(i)
+
+        else:
+            if current > top_1:
+                top_2 = top_1
+                top_1 = current
+            elif current > top_2 and current < top_1:
+                top_3 = top_2
+                top_2 = current
+            elif current > top_3 and current < top_2:
+                top_3 = current
+            current = 0
+    return top_1 + top_2 + top_3
+
+print(top_3_calories(calories))
