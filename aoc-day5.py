909d58beabcca9da06cf0b224b07665286bac019
Ektor Nikolinakos <hc.nlks@gmail.com>
2022-12-26 17:21:21 +0200

    aoc day 5

--- aoc-day5.py
+++ aoc-day5.py
@@ -14,34 +14,72 @@ stacks = data_clean[0:8] # separate stacks from moves
 
 stacks_dict = dict() 
 
-# Creates keys equal to the number of stacks in the data 
-for i in range(1, len(data_clean[0][1:-1][::4])+1):
-    stacks_dict[i] = []
+def vintage_crane(data_clean):
+    # Creates keys equal to the number of stacks in the data 
+    for i in range(1, len(data_clean[0][1:-1][::4])+1):
+        stacks_dict[i] = []
 
-# keys represent each stuck and their values which items are in each stack
-for i in stacks:
-    for i, j in enumerate(i[1:-1][::4]):
-        if j != " ":
-            stacks_dict[i+1] += [j]
+    # keys represent each stuck and their values which items are in each stack
+    for i in stacks:
+        for i, j in enumerate(i[1:-1][::4]):
+            if j != " ":
+                stacks_dict[i+1] += [j]
 
-moves = data_clean[10:-1] # separate moves from the text file
+    moves = data_clean[10:-1] # separate moves from the text file
 
-for i in moves:
-    # Split each move for every space so we can get the numbers indicating the stacks
-    i = i.split(" ")
+    for i in moves:
+        # Split each move for every space so we can get the numbers indicating the stacks
+        i = i.split(" ")
 
-    # Get the items from the top of the stack, reverse order, then add items at the beginning of the stack indicated
-    stacks_dict[int(i[5])] = stacks_dict[int(i[3])][:int(i[1])][::-1] + stacks_dict[int(i[5])]  
-    
-    # Remove items from the first stack
-    del stacks_dict[int(i[3])][:int(i[1])]
+        # Get the items from the top of the stack, reverse order, then add items at the beginning of the stack indicated
+        stacks_dict[int(i[5])] = stacks_dict[int(i[3])][:int(i[1])][::-1] + stacks_dict[int(i[5])]  
 
-answer = "" # empty string to store the answer
+        # Remove items from the first stack
+        del stacks_dict[int(i[3])][:int(i[1])]
 
-# add the top element of each stack to the answer 
-for i in range(1, len(stacks_dict)+1):
-    answer += stacks_dict[i][0]
+    answer = "" # empty string to store the answer
 
-print(answer)
+    # add the top element of each stack to the answer 
+    for i in range(1, len(stacks_dict)+1):
+        answer += stacks_dict[i][0]
+
+    return answer
+
+# print(vintage_crane(data_clean))
+# JRVNHHCSJ
 
 # Part 2 ############################################### (in progress)
+
+def eva_unit_05(data_clean):
+    """Same as above - just removed "[::-1]" from line 72"""
+    # Creates keys equal to the number of stacks in the data 
+    for i in range(1, len(data_clean[0][1:-1][::4])+1):
+        stacks_dict[i] = []
+
+    # keys represent each stuck and their values which items are in each stack
+    for i in stacks:
+        for i, j in enumerate(i[1:-1][::4]):
+            if j != " ":
+                stacks_dict[i+1] += [j]
+
+    moves = data_clean[10:-1] # separate moves from the text file
+
+    for i in moves:
+        # Split each move for every space so we can get the numbers indicating the stacks
+        i = i.split(" ")
+
+        # Get the items from the top of the stack, reverse order, then add items at the beginning of the stack indicated
+        stacks_dict[int(i[5])] = stacks_dict[int(i[3])][:int(i[1])] + stacks_dict[int(i[5])]  
+
+        # Remove items from the first stack
+        del stacks_dict[int(i[3])][:int(i[1])]
+
+    answer = "" # empty string to store the answer
+
+    # add the top element of each stack to the answer 
+    for i in range(1, len(stacks_dict)+1):
+        answer += stacks_dict[i][0]
+
+    return answer
+
+print(eva_unit_05(data_clean))
